e8cee6333a319dbdbef4cbd4e005e589bc66d563
needarling1 <97009120+needarling1@users.noreply.github.com>
2024-09-05 23:00:18 -0700

    set up project - vite, react, tailwind, flask

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './features/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      <div className = "w-full h-full">
+        <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
