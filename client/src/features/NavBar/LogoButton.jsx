095dc3a89b714cb7861efbb420415fb8f53bed17
Alex K. <97009120+needarling1@users.noreply.github.com>
2025-07-09 16:05:04 -0700

    Update LogoButton.jsx

--- client/src/features/NavBar/LogoButton.jsx
+++ client/src/features/NavBar/LogoButton.jsx
@@ -4,7 +4,6 @@ const LogoButton = () => {
     return (
       <div>
         <Link className = "flex h-full overflow-hidden items-center relative text-start text-l md:text-l l:text-xl xl:text-xl 2xl:text-2xl 3xl:text-2xl 4xl:text-2xl py-3 cursor-pointer rounded-xl" to = "/">
-        <img className = "h-[3vh] w-auto mx-1.5" src = "https://static-00.iconduck.com/assets.00/vault-icon-512x497-k48mdkzv.png" alt = "logo"/>
         IB Vault
         </Link>
       </div>
